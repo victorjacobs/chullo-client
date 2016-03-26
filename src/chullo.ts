f4a3f556b964b5eff850292dca113345fb3c3a69
Victor Jacobs <victor.jacobs@me.com>
2016-03-26 21:45:18 +0100

    Progressbar + remove after upload

--- src/chullo.ts
+++ src/chullo.ts
@@ -1,9 +1,12 @@
+import * as fs from 'fs';
 import * as chokidar from 'chokidar';
 import * as request from 'superagent';
 import {Promise} from 'es6-promise';
+import * as ProgressBar from 'progress';
 import {Clipboard} from './clipboard';
 
 export class Chullo {
+    // TODO more properly check responses from api (through status codes)
     // TODO factor these out to environment-dependent things
     private accessToken: Promise<string>;
     private clientId: string;
@@ -40,7 +43,7 @@ export class Chullo {
         });
     }
 
-    watch(directory: string, pattern?: string, removeAfterUpload?: boolean):void {
+    watch(directory: string, pattern?: string, removeAfterUpload?: boolean): void {
         let watchPattern = directory;
         if (pattern) {
             watchPattern += '/' + pattern;
@@ -52,16 +55,19 @@ export class Chullo {
         });
 
         watcher.on('add', path => {
-            this.upload(directory + '/' + path);
+            this.upload(directory + '/' + path).then(() => {
+                if (removeAfterUpload) {
+                    fs.unlinkSync(path);
+                }
+            });
         })
     }
 
-    upload(file: string) {
-        this.accessToken.then(accessToken => {
-            console.log('uploading ' + file + ' accessToken ' + accessToken);
+    upload(file: string): Promise<void> {
+        return this.accessToken.then(accessToken => {
+            console.log('Starting upload of ' + file);
             // First do a POST to get a unique url to upload to
             return new Promise((resolve, reject) => {
-                console.log('Creating file');
                 request
                     .post(this.endpoint + '/files')
                     .set('Authorization', 'Bearer ' + accessToken)
@@ -74,26 +80,35 @@ export class Chullo {
                             reject('Error posting to files: ' + err);
                         }
 
-                        console.log('View URL: ' + response.body);
+                        console.log('File id ' + response.body._id + ' view URL: ' + response.body.viewUrl);
+                        Clipboard.copy(response.body.viewUrl);
                         resolve(response);
                     })
                 ;
             })
         }).then((response: request.Response) => {
-            console.log('Uploading file');
             // Then upload the file
             this.accessToken.then(accessToken => {
+                let bar = new ProgressBar('Uploading [:bar] :percent :etas', {
+                    complete: '=',
+                    incomplete: ' ',
+                    width: 20,
+                    total: 100
+                });
+
                 request
                     .post(this.endpoint + '/upload/' + response.body._id)
                     .set('Authorization', 'Bearer ' + accessToken)
                     .attach('file', file)
+                    .on('progress', e => {
+                        let progress = Math.round((e.loaded / e.total) * 100);
+                        bar.tick(progress);
+                    })
                     .end((err, response) => {
                         if (err) {
                             // TODO this reject doesnt work yet apparently
                             return Promise.reject(err);
                         }
-
-                        console.log(response.body);
 
                         return response;
                     })
